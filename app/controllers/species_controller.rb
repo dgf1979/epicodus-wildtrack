27ecae3d44724bc35932a100b8650d14823f4c61
drew & chris <student@epicodus.com>
2015-06-29 12:13:18 -0700

    add species model; ability to create and list species

---
+++ app/controllers/species_controller.rb
@@ -0,0 +1,29 @@
+class SpeciesController < ApplicationController
+  def index
+    @species = Species.all
+    render :index
+  end
+
+  def show
+
+  end
+
+  def new
+    @species = Species.new
+    render :new
+  end
+
+  def create
+    @species = Species.new(species_params)
+    if @species.save
+      redirect_to species_index_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def species_params
+    params.require(:species).permit(:name)
+  end
+end
