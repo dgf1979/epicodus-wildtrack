f9b612fd9baf488978b08a14c2bf6f78ff21d696
drew & chris <student@epicodus.com>
2015-06-29 12:30:34 -0700

    enable editing of species names

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -14,6 +14,20 @@ class SpeciesController < ApplicationController
     render :new
   end
 
+  def edit
+    @species = Species.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @species = Species.find(params[:id])
+    if @species.update(species_params)
+      redirect_to species_index_path
+    else
+      render :edit
+    end
+  end
+
   def create
     @species = Species.new(species_params)
     if @species.save
